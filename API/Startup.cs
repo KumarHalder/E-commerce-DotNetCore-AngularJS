fea9749c163c4a85b497c1293d8283019e7d259d
Kumar Halder <khalder@ualberta.ca>
2021-01-04 13:59:29 +0600

    Product detail, description, producttype, brands addded. Database updated with seeded data

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
 
+using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -11,19 +13,18 @@ namespace API
 {
     public class Startup
     {
-       
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
             _config = config;
         }
 
-        public IConfiguration Configuration { get; }
+       // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<Infrastructure.Data.StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("Default")));
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandsData = 
+                        File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    foreach (var item in brands)
+                    {
+                          context.ProductBrands.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
